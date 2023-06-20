d51c3933040a1af40292065df658a7ebb77bbcb7
rachman rachman <rachman456258@gmail.com>
2023-06-20 19:58:53 +0700

    feature Orderlist Costumer

---
+++ src/page/HalamanOrderlistCos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationCos from '../commponent/NavigationCos'
+import OrderlistCos from '../commponent/OrderlistCos'
+
+const HalamanOrderlistCos = () => {
+  return (
+    <div>
+        <NavigationCos/>
+        <OrderlistCos/>
+    </div>
+  )
+}
+
+export default HalamanOrderlistCos
